bec1d2f64598fa7a0629af9c17f8473524178733
tklein23 <tklein23@users.noreply.github.com>
2014-07-27 21:31:28 +0200

    Removing MacOS X related stuff from cmake line.

--- scripts/setup-shogun.sh
+++ scripts/setup-shogun.sh
@@ -42,7 +42,8 @@ if [ "$(uname)" == "Darwin" ] && type brew >/dev/null 2>&1; then
 		CMAKE_EXTRA_OPTS="-DPYTHON_INCLUDE_DIR=${BREW_PYTHON_HEADERS} -DPYTHON_LIBRARY=${BREW_PYTHON_LIBRARY} "
 	fi
 fi
-cmake $CMAKE_EXTRA_OPTS -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Headers -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.8/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DPythonModular=ON -DJavaModular=ON -DCMAKE_INSTALL_PREFIX="./install" ..
+cmake $CMAKE_EXTRA_OPTS -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON \
+	-DPythonModular=ON -DJavaModular=ON -DCMAKE_INSTALL_PREFIX="./install" ..
 
 ### building, installing and testing (HACK: due to a bug, locale needs to be C)
 time make -j$NUM_JOBS all
